c1e4bc89f7067da96f6174a8379375036960d7a5
Nick Dobrynin <dobryninnick1@gmail.com>
2022-12-11 19:01:35 +0600

    Send message fix

--- src/common/Chat/ChatInput/ChatInput.tsx
+++ src/common/Chat/ChatInput/ChatInput.tsx
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 import React, {useState} from 'react';
-import {useApolloClient, useMutation, useQuery} from '@apollo/client';
+import {useMutation} from '@apollo/client';
 import SEND_MESSAGE from '../../../apollo/api/sendMessage';
 import {Chat} from '../../../../types';
 import GET_CHATS from '../../../apollo/api/getChats';
-import GET_USER from '../../../apollo/api/getUser';
 
 const Input = styled.input`
   width: 100%;
@@ -32,8 +31,6 @@ interface IChatInput {
 }
 
 const ChatInput: React.FC<IChatInput> = ({chatId, to}) => {
-  const user = useQuery(GET_USER);
-  const client = useApolloClient();
   const [sendMessage] = useMutation(SEND_MESSAGE);
   const [inputValue, setInputValue] = useState<string>('');
   const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -43,48 +40,25 @@ const ChatInput: React.FC<IChatInput> = ({chatId, to}) => {
   const onSubmit = async (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       if (inputValue.trim()) {
-        const currentChats = client.readQuery<IChats>({query: GET_CHATS})!.getChats;
-        client.writeQuery({
-          query: GET_CHATS,
-          data: {
-            gerChats: [...currentChats.map((chat: Chat) => {
-              if (chat.id === chatId) {
-                console.log({...chat, messages: [...chat.messages, {
-                  __typename: 'Message',
-                  id: ++chat.messages.length,
-                  from: user && user?.data?.getUser?.username,
-                  message: inputValue
-                }]})
-                return {...chat, messages: [...chat.messages, {...chat, messages: [...chat.messages, {
-                      __typename: 'Message',
-                      id: ++chat.messages.length,
-                      from: user && user?.data?.getUser?.username,
-                      message: inputValue
-                    }]}]}
-              }
-              else return chat;
-            })]
-          }
-        })
-        // await sendMessage({
-        //   variables: {
-        //     message: inputValue,
-        //     to,
-        //     chatId
-        //   },
-        //   update: (cache, {data: {sendMessage}}) => {
-        //     const currentChats = cache.readQuery<IChats>({query: GET_CHATS})!.getChats;
-        //     cache.writeQuery({
-        //       query: GET_CHATS,
-        //       data: {
-        //         getChats: [...currentChats.map((chat: Chat) => {
-        //           if (chat.id === sendMessage.id) return sendMessage;
-        //           else return chat;
-        //         })]
-        //       },
-        //     })
-        //   },
-        // });
+        await sendMessage({
+          variables: {
+            message: inputValue,
+            to,
+            chatId
+          },
+          update: (cache, {data: {sendMessage}}) => {
+            const currentChats = cache.readQuery<IChats>({query: GET_CHATS})!.getChats;
+            cache.writeQuery({
+              query: GET_CHATS,
+              data: {
+                getChats: [...currentChats.map((chat: Chat) => {
+                  if (chat.id === sendMessage.id) return sendMessage;
+                  else return chat;
+                })]
+              },
+            })
+          },
+        });
         setInputValue('');
       }
     }
